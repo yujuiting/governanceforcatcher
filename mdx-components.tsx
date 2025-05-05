5f13215a40ff1705e5703d24c5f8394a38523e7d
yujuiting <yujuiting@me.com>
2025-05-05 21:10:55 +0800

    update

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -4,17 +4,17 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
     h1: ({ children, ...props }) => (
-      <h1 className="text-5xl text-[#0C2836] text-center my-2.5 font-bold border-t-4 border-b-4 border-[#17506C] py-2.5" {...props}>
+      <h1 className="text-5xl text-[#0C2836] text-center my-2.5 font-tauri font-bold border-t-4 border-b-4 border-[#17506C] py-2.5" {...props}>
         {children}
       </h1>
     ),
     h2: ({ children, ...props }) => (
-      <h2 className="text-3xl text-[#17506C] text-center my-2.5" {...props}>
+      <h2 className="text-3xl text-[#17506C] text-center my-2.5 font-tauri" {...props}>
         {children}
       </h2>
     ),
     a: ({ children, ...props }) => (
-      <a {...props} target="_blank" rel="noopener noreferrer">
+      <a {...props} target="_blank" rel="noopener noreferrer" className="font-tauri">
         {children}
       </a>
     ),
